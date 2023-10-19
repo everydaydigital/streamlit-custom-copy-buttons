600f2bb60c289f613e290891317d4f73ef5b63de
everydaydigital <12283888+everydaydigital@users.noreply.github.com>
2023-10-19 14:25:35 +1000

    Update copy.py

--- copy.py
+++ copy.py
@@ -1,7 +1,8 @@
 import streamlit as st
 
-text_to_copy = "Hello, World!"
-hosted_html_file = https://everydayswag.org/files/copy.html
-iframe_url = f"{hosted_html_file }?copy={text_to_copy }"
+text_to_copy = st.text_input("Hello, World!")
+
+hosted_html_file = "https://everydayswag.org/files/copy.html"
+iframe_url = f"{hosted_html_file}?copy={text_to_copy}"
 
 st.markdown(f'<iframe src="{iframe_url}"></iframe>', unsafe_allow_html=True)
