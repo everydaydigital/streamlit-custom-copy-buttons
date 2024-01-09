237beba5113b049440e7726b84a7e625b32d993a
everydaydigital <12283888+everydaydigital@users.noreply.github.com>
2024-01-09 13:03:11 +1000

    Update copy.py
    'overflow: hidden' helps to ensure that the scrollbars are not visible when viewing the copy button as an embeded iframe on chrome

--- copy.py
+++ copy.py
@@ -5,4 +5,4 @@ text_to_copy = st.text_input("Hello, World!")
 hosted_html_file = "https://everydayswag.org/files/copy.html"
 iframe_url = f"{hosted_html_file}?copy={text_to_copy}"
 
-st.markdown(f'<iframe src="{iframe_url}"></iframe>', unsafe_allow_html=True)
+st.markdown(f'<iframe style="overflow: hidden;" src="{iframe_url}"></iframe>', unsafe_allow_html=True)
